6b56f6603d798f01f2be8c3c15c13d9d5f181430
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 15:19:20 -0400

    Add static files.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -154,7 +154,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 import django_heroku
 django_heroku.settings(locals())
