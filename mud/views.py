3a5e1920206cb39a8f5272d5154ab77ce2fb0d09
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 16:55:33 -0400

    Update index view.

--- mud/views.py
+++ mud/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts	import render
 from django.http		import HttpResponse
+from decouple			import config
 
 
 def index(request):
-	return HttpResponse("Hello! You're at the MUD index. The API is up an running.")
+	front_end_url = config('CORS_ORIGIN_WHITELIST')
+	in_dev_env = config('DEBUG')
+	protocol = 'http://' if in_dev_env else 'https://'
+	return HttpResponse("""
+		<h1 style = 'text-align: center'>Hello! You\'re at the MUD index. The API is up an running.</h1>
+		<h2 style = 'text-align: center'>Checkout the front-end at:</h2>
+		<h2 style = 'text-align: center'>{}{}</h2>
+	""".format(protocol, front_end_url))
