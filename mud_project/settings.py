612ceed0595a1761a9985a222a0dbf818f81bdf0
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 15:40:40 -0400

    Update settings file.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -161,18 +161,30 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL ='/static/'
+# # MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
+# # MEDIA_URL = "/media/"
+# # STATICFILES_DIRS = (
+# # os.path.join(BASE_DIR, 'staticfiles'),
+# # )
+
+# if DEBUG is False: # if in production, use this static root
+#     STATIC_ROOT = '/app/static/'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL ='/static/'
-# MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
-# MEDIA_URL = "/media/"
-# STATICFILES_DIRS = (
-# os.path.join(BASE_DIR, 'staticfiles'),
-# )
+STATIC_URL = '/static/'
 
-if DEBUG is False: # if in production, use this static root
-    STATIC_ROOT = '/app/static/'
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 import django_heroku
 django_heroku.settings(locals())
