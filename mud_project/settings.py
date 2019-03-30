6d2d5aa926cccf67261861a45885e33973f1b38d
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 15:34:14 -0400

    Remove variable.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -166,9 +166,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL ='/static/'
 # MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
 # MEDIA_URL = "/media/"
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+# os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
 if DEBUG is False: # if in production, use this static root
     STATIC_ROOT = '/app/static/'
