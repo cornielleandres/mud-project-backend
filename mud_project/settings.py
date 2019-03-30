2d237a4c9fe857d1b61b9874e55c8e79e613b85b
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 15:31:30 -0400

    Update settings for whitenoise.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -153,13 +153,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL ='/static/'
+# MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
+# MEDIA_URL = "/media/"
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
+
+if DEBUG is False: # if in production, use this static root
+    STATIC_ROOT = '/app/static/'
+
 
 import django_heroku
 django_heroku.settings(locals())
