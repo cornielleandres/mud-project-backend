8fa264cf6e2e2b5a160ae251f2bcc9c2bd6084f8
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 16:15:30 -0400

    Move staticfiles to front-end.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -153,40 +153,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL ='/static/'
-# # MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
-# # MEDIA_URL = "/media/"
-# # STATICFILES_DIRS = (
-# # os.path.join(BASE_DIR, 'staticfiles'),
-# # )
-
-# if DEBUG is False: # if in production, use this static root
-#     STATIC_ROOT = '/app/static/'
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
 
 import django_heroku
 django_heroku.settings(locals())
-
-# del DATABASES['default']['OPTIONS']['sslmode']
