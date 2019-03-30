ce495d114e7c22d54403bc7e98e6532060434d67
Carlos Vargas <cornielleandres@gmail.com>
2019-03-30 15:46:48 -0400

    Remove static folder.

--- mud_project/settings.py
+++ mud_project/settings.py
@@ -181,10 +181,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 import django_heroku
 django_heroku.settings(locals())
